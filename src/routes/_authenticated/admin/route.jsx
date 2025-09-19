9fd9650b869c793bae508dc0dc64f76dce89c7ab
huzaifa321s <muhammadhuzaifa@humantekteam.com>
2025-09-19 12:01:18 +0500

    route.jsx admin dashboard updated

--- src/routes/_authenticated/admin/route.jsx
+++ src/routes/_authenticated/admin/route.jsx
@@ -4,13 +4,13 @@ import { AuthenticatedLayout } from './layout/admin-authenticated-layout'
 import { initializeAxios } from './-utils/InitializeAxios'
 
 function AdminRouteComponent() {
-  useEffect(() => {
-    initializeAxios()
-  }, [])
 
   return <AuthenticatedLayout />
 }
 
 export const Route = createFileRoute('/_authenticated/admin')({
+  beforeLoad:() =>{
+initializeAxios()
+  },
   component: AdminRouteComponent,
 })
